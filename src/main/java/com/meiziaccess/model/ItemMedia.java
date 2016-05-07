23b8095f273f8675e4f28f214e49b3f8ac318aae
lihongqiang <415200973@qq.com>
2016-05-07 15:05:08 +0800

    update gitignore

--- src/main/java/com/meiziaccess/model/ItemMedia.java
+++ src/main/java/com/meiziaccess/model/ItemMedia.java
@@ -1,24 +1,36 @@
 package com.meiziaccess.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by user-u1 on 2016/4/14.
  */
-//获取Post过来的订单数据
-public class Order {
 
+//获取Post过来的订单数据
+@Entity
+@Table(name = "item_media")
+public class ItemMedia {
+
+    @Column(name = "uuid")
     private Long uuid;
 
+    @Column(name = "isEntire")
     private boolean isEntire;
 
+    @Column(name = "starttime")
     private Long starttime;
 
+    @Column(name = "endtime")
     private Long endtime;
 
+    @Column(name = "highdef_video_path")
     private String highdef_video_path;
 
-    public Order() {super();}
+    public ItemMedia() {super();}
 
-    public Order(Long uuid, boolean isEntire, Long starttime, Long endtime, String highdef_video_path) {
+    public ItemMedia(Long uuid, boolean isEntire, Long starttime, Long endtime, String highdef_video_path) {
         super();
         this.uuid = uuid;
         this.isEntire = isEntire;
